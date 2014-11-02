db9bb644845805b6cc1e947826fe3364a5d4075d
rafistern1986 <rsternbuch@gmail.com>
2014-11-02 21:31:27 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -10,6 +10,7 @@ ctx.lineTo(160, 25);
 ctx.lineTo(160, 40);
 
 //פאות
+
 ctx.moveTo(95, 40);
 ctx.lineTo(95, 85);
 ctx.moveTo(165, 40);
@@ -31,12 +32,17 @@ ctx.lineTo(160, 70);
 ctx.lineTo(160, 50);
 ctx.moveTo(150, 60);
 ctx.lineTo(152, 60);
-
+ctx.stroke();
 //nose
+ctx.beginPath();
+ctx.lineWidth = "3";
+ctx.strokeStyle = "green";
 ctx.moveTo(130, 65);
 ctx.lineTo(130, 80);
-
+ctx.stroke();
 //happy mouse
+ctx.beginPath();
+ctx.strokeStyle = "black";
 ctx.moveTo(110, 90);
 ctx.lineTo(110, 95);
 ctx.lineTo(150, 95);
@@ -44,19 +50,9 @@ ctx.lineTo(150, 90);
 
 ctx.stroke();
 
-
-function swithFace() {
-    var valueOfButton = document.getElementById("lol").value;
-    if (valueOfButton == "swith to litvak") {
-        ctx.clearRect(0, 0, c.width, c.heictxt);
-        litwak();
-    } else {
-        chasid()
-    }
-};
-
-
 function chasid() {
+    var buttonValue = document.getElementById("lol");
+    buttonValue.value = "swith to litvak";
     //hat
     ctx.moveTo(75, 40);
     ctx.lineTo(185, 40);
@@ -147,4 +143,16 @@ function litwak() {
     ctx.lineTo(150, 100);
 
     ctx.stroke();
+};
+
+
+function swithFace() {
+    var valueOfButton = document.getElementById("lol").value;
+    if (valueOfButton == "swith to litvak") {
+        ctx.clearRect(0, 0, c.width, c.height);
+        litwak();
+    } else if (valueOfButton == "swith to casid") {
+        ctx.clearRect(0, 0, c.width, c.height);
+        chasid();
+    }
 };
